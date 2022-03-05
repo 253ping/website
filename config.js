f2d28a4dbf6755a3e6f6c117ea84830d2de2643b
VanillaCraftDE <75546957+VanillaCraftDE@users.noreply.github.com>
2022-03-05 15:55:08 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+/*!
+ * Quara Theme by OllieJW (https://olliejw.me)
+ * License - https://olliejw.me/tos
+ */
+
+var server_name = "VanillaCraft";
+var server_ip = "mc.vanillacraft.one";
+var server_port = 25565;
+var ip_copied = "IP copied!";
+
+var firefly_count = 20;
+
+staff("Laurino", "4ffcbe16-9da7-4aec-b6b4-55b7091ba7f6", "Founder / Owner / Admin");
+staff("Friedrich", "be48b70f-d0c9-4a51-ab90-4295ed8a87c3", "Owner / Admin");
+staff("Kxwaiix", "31d85029-c0e5-4d96-86b7-ee5ceb8b3e8d", "Admin");
+
+
+vote("Staffel 3", "./img/discord3.png", "https://mega.nz/file/uPgQxTTb#f3x1YXXW9N8pNi1gwabP-IpzTFL2_Ho9Fl_EGbL2JLI");
+vote("Staffel 2", "./img/discord2.png", "https://mega.nz/file/jDRgVTLQ#uEu3NrkQeRH-Y9nrUobn-bGEWdIRWCP-KnySkYyJe7A");
+vote("Staffel 1", "./img/discord1.png", "https://mega.nz/file/Peh0WKJK#28XbZAGKKppGUYETezMTuWHijSU_-a5j4dKvMCQ6KPk");
