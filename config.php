e3ed5416e66b19db0a898142fe1b4e3247e78757
VanillaCraftDE <75546957+VanillaCraftDE@users.noreply.github.com>
2022-03-11 11:44:03 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,33 @@
+<?php
+
+/*******************************************************
+ * Copyright (C) 2020 Nicolas Proske (AKA pume)
+ * Twitter: https://twitter.com/einpume
+ *
+ * This file is part of "PORTAL!".
+ *
+ * "PORTAL!" can not be distributed without
+ * the express permission of Nicolas Proske (AKA pume)
+ *
+ * This copyright notice may not be removed or changed.
+ *******************************************************/
+
+/* SERVER INFO */
+$server_ip = 'mc.vanillacraft.one';
+
+/* HEADER TEXT */
+$header_title = 'VanillaCraft.one';
+$header_caption = 'Welcome to our "PORTAL!" website';
+
+/* SOCIAL MEDIA LINKS */
+$twitter_link = 'https://twitter.com/einpume';
+$teamspeak_ip = 'YourServer.net';
+$discord_link = 'http://discord.gg/saxjW7H9wp';
+
+/* SERVER INFO TEXT */
+$server_info_loading = 'Loading server data.. ';
+$server_info_server_offline = 'Server is offline';
+
+/* FOOTER TEXT */
+$footer_imprint_link = 'https://yourserver.net/imprint';
+$footer_imprint_text = 'Imprint & Privacy Policy';
